358f97c7bd9ae8f6147febc15bd78fabc8cf4370
jinsy <jinsy@qburst.com>
2016-02-05 12:14:40 +0530

    status form

--- app/controllers/StatusController.js
+++ app/controllers/StatusController.js
@@ -18,8 +18,6 @@ $scope.activity_list = null;
             $scope.activity_list = [{heading:"Error",description:"Could not load json   data"}];
         });
 
-//Creating hours
-
     $scope.hours = [];
 
     var i =0;
@@ -28,8 +26,6 @@ $scope.activity_list = null;
         $scope.hours.push({id: i, value: i});
     }
 
-    //Creating Minutes
-
     $scope.minutes = [];
     i =0;
     for(i=0; i<4; i++)
@@ -37,22 +33,20 @@ $scope.activity_list = null;
         $scope.minutes.push({id: i, value: i*15});
     }
 
-    //Injecting Dates to the date column
     $scope.dates = [];
 
-    var curr = new Date(); // get current date
-    var first = curr.getDate(); // First day is the day of the month - the day of the week
-    // var last = first + 6; // last day is the first day + 6
-    var startDate = new Date(curr.setDate(first));
-    startDate = "" + startDate.getFullYear() +"-" +(startDate.getMonth() + 1) + "-" + startDate.getDate() ;
+    var currentDate = new Date(); 
+    var first = currentDate.getDate(); 
+    var startDate = new Date(currentDate.setDate(first));
+    startDate = startDate.getDate()+"-" +(startDate.getMonth() + 1) + "-" + + startDate.getFullYear()  ;
 
     i = 0;
 
-    for(i=0; i<7;i++)
+    for(i=0; i<10;i++)
     {
         var last = first-i;
-        var endDate = new Date(curr.setDate(last));
-        endDate = endDate.getFullYear() +"-" +(endDate.getMonth() + 1) + "-" + endDate.getDate();
+        var endDate = new Date(currentDate.setDate(last));
+        endDate = endDate.getDate()+"-" +(endDate.getMonth() + 1) + "-" +endDate.getFullYear()  ;
 
         $scope.dates.push({
             id: i,
